0f25183b289acce99907eb0408ab83fa0804e35d
Natã Elienai <nata.elienai@gmail.com>
2021-12-01 17:38:41 -0300

    test: add tests to the FavoritePokemons component

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+test('Exibe a mensagem "No favorite pokemon found" se não há pokémons favoritos', () => {
+  render(<FavoritePokemons />);
+  const message = screen.getByText(/no favorite pokemon found/i);
+
+  expect(message).toBeDefined();
+});
+
+test('Exibe todos os cards de pokémons favoritados', () => {
+  render(
+    <BrowserRouter>
+      <FavoritePokemons pokemons={ pokemons } />
+    </BrowserRouter>,
+  );
+
+  pokemons.forEach(({ name }) => {
+    const nameElement = screen.getByText(name);
+    expect(nameElement).toBeDefined();
+  });
+});
